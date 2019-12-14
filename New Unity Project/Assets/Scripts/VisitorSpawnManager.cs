4e406b86c81fa3c018c08590b9ce597ba5469404
Shortiez <54028203+Shortiez@users.noreply.github.com>
2019-12-14 19:59:17 +0000

    Added Visitor Spawning
    Added Visitor Spawning

---
+++ New Unity Project/Assets/Scripts/VisitorSpawnManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisitorSpawnManager : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    public GameObject visitor;
+
+    public float timeBetweenSpawns = 8f;
+    private float countdown;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            SpawnVisitor();
+            countdown = timeBetweenSpawns;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    public void SpawnVisitor() 
+    {
+        Instantiate(visitor, spawnPoint.position, Quaternion.identity);
+    }
+}
