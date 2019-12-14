7b1225c9e1e95a974f746412ea7d7e1a77ccbce6
Shortiez <54028203+Shortiez@users.noreply.github.com>
2019-12-14 20:55:30 +0000

    Very Basic Pathfinding

--- New Unity Project/Assets/Scripts/Visitor/Visitor.cs
+++ New Unity Project/Assets/Scripts/Visitor/Visitor.cs
@@ -5,16 +5,44 @@ using UnityEngine.AI;
 
 public class Visitor : MonoBehaviour
 {
+    [SerializeField]
+    private Transform destination;
 
-    // Start is called before the first frame update
+    NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        destination = GameObject.FindGameObjectWithTag("Destination").transform;
+    }
+
     void Start()
+    {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if (!navMeshAgent)
+        {
+            Debug.Log("NavMeshAgent not added to" + gameObject.name);
+        }
+        else
+        {
+            SetDestination();
+        }
+
+       
+    }
+
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetDestination()
     {
-        
+        if (destination)
+        {
+            Vector3 targetDestination = destination.transform.position;
+            navMeshAgent.SetDestination(targetDestination);
+        }
     }
 }
